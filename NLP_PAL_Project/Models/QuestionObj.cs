32ffaf9b1a3cb87cffb31f2351ded305f639dfd5
DanZlotnikov <danzlotnikov@gmail.com>
2024-07-04 17:07:36 +0300

    c

---
+++ NLP_PAL_Project/Models/QuestionObj.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP_PAL_Project.Models
+{
+    internal class QuestionObj
+    {
+        public int Id { get; set; }
+        public int Question { get; set; }
+        public int Answer { get; set; }
+    }
+}
--- NLP_PAL_Project/Program.cs
+++ NLP_PAL_Project/Program.cs
@@ -1,11 +1,23 @@
-﻿namespace NLP_PAL_Project
+﻿using NLP_PAL_Project.Models;
+
+namespace NLP_PAL_Project
 {  
     class Program
     {
-       
         public static async Task Main(string[] args)
         {
             Consts.Init();
+            // Read all prompt data - Bar
+            List<QuestionObj> list = new List<QuestionObj>();
+
+            // Send prompt to GPT and get code - Dan
+
+            // Execute code on compilers - Denis
+
+            // Get answers from compilers and give scores to languages
+
+            // Graphs, stats, etc.
+
             var ret = await GptLogic.ProcessCompletionRequest(new Models.RequestCompletionParam());
             Console.WriteLine(ret);
         }
