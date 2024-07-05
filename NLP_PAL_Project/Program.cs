ba1bfe7890bd8d30db19ad65e541d4cbd1f5c139
barKyzer <82834578+barKyzer@users.noreply.github.com>
2024-07-05 04:09:16 -0600

    Update Program.cs
    BAR FRIDAY

--- NLP_PAL_Project/Program.cs
+++ NLP_PAL_Project/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 
 using NLP_PAL_Project.Models;
-
 namespace NLP_PAL_Project
 {  
     class Program
@@ -10,16 +9,7 @@ namespace NLP_PAL_Project
         {
             //Consts.Init();
             // Read all prompt data - Bar
-            List<QuestionObj> questionObjs = new List<QuestionObj> {
-                new QuestionObj
-                {
-                    Id = 1,
-                    Intro = "",
-                    ExampleQuestion = "Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?",
-                    ExampleAnswer = "A: # solution in Python:\r\n\"\"\"There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?\"\"\"\r\ncomputers_initial = 9\r\ncomputers_per_day = 5\r\nnum_days = 4  # 4 days between monday and thursday\r\ncomputers_added = computers_per_day * num_days\r\ncomputers_total = computers_initial + computers_added\r\nresult = computers_total\r\nreturn result",
-                    RealQuestion= "Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?",
-                }
-            };
+            List<QuestionObj> questionObjs = await ReadQuestionsFromJsonAsync("/mnt/data/test.jsonl");
 
             //dynamic[] codeSnippets = await GptLogic.GeneratePalAnswers(questionObjs);
 
@@ -56,5 +46,36 @@ namespace NLP_PAL_Project
 
 
         }
+
+    
+        public static async Task<List<QuestionObj>> ReadQuestionsFromJsonAsync(string filePath)
+        {
+            List<QuestionObj> questionObjs = new List<QuestionObj>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                int id = 1;
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    var data = JsonSerializer.Deserialize<Dictionary<string, string>>(line);
+
+                    QuestionObj questionObj = new QuestionObj
+                    {
+                        Id = id,
+                        ExampleQuestion = "Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?",
+                        ExampleAnswer = "A: # solution in Python:\r\n\"\"\"There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?\"\"\"\r\ncomputers_initial = 9\r\ncomputers_per_day = 5\r\nnum_days = 4  # 4 days between monday and thursday\r\ncomputers_added = computers_per_day * num_days\r\ncomputers_total = computers_initial + computers_added\r\nresult = computers_total\r\nreturn result",
+                        Realuestion = data["question"],
+                        RealAnswer = data["answer"]
+                    };
+
+                    questionObjs.Add(questionObj);
+                    id++;
+                }
+            }
+
+            return questionObjs;
+        }
+        
     }
 }
